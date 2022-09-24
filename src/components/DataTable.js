e63a8c58704a81944aac643b85b888875243965a
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-09-24 19:11:37 +0200

    declare DataTable

---
+++ src/components/DataTable.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DataTable = () => {
+  return (
+    <div className="grid grid-cols-3">
+      <div>col 1</div>
+      <div>col 2</div>
+      <div>col 3</div>
+    </div>
+  )
+}
+
+export default DataTable
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import DataTable from '../components/DataTable'
 
 const HomePage = () => {
   return (
     <div>
-      Home page!
+      <DataTable />
     </div>
   )
 }

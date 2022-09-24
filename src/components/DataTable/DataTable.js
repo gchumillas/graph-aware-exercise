9af9e83156e3cc04dd7fe635440c2716e7823a3a
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-09-24 23:13:47 +0200

    add DataTableContainer

--- src/components/DataTable/DataTable.js
+++ src/components/DataTable/DataTable.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 import tw from 'twin.macro'
-import { context } from './context'
 import DataRow from './DataRow'
 
 /**
@@ -16,30 +15,25 @@ import DataRow from './DataRow'
  * @param {TableRow[]} params.rows
  * @param {(id: string) => void} params.onDeleteRow
  */
-const DataTable = ({ rows, onDeleteRow }) => {
-  const contextValue = React.useMemo(() => ({
-    deleteRow: onDeleteRow
-  }), [onDeleteRow])
+const DataTable = ({ rows }) => {
   const columns = Object.keys(rows[0]?.data || [])
 
   // the ID is always the first column
   const columnId = columns[0]
 
   return (
-    <context.Provider value={contextValue}>
-      <StyledTable>
-        <thead>
-          <tr>
-            <th>&nbsp;</th>
-            {columns.map((column) => <th key={column}>{column}</th>)}
-            <th>&nbsp;</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows.map((row) => <DataRow key={row.data[columnId]} row={row} />)}
-        </tbody>
-      </StyledTable>
-    </context.Provider>
+    <StyledTable>
+      <thead>
+        <tr>
+          <th>&nbsp;</th>
+          {columns.map((column) => <th key={column}>{column}</th>)}
+          <th>&nbsp;</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((row) => <DataRow key={row.data[columnId]} row={row} />)}
+      </tbody>
+    </StyledTable>
   )
 }
 
---
+++ src/components/DataTable/DataTableContainer.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { context } from './context'
+import DataTable from './DataTable'
+
+/**
+ * @typedef {{
+ *  data: Record<string, string>,
+ *  kids: Record<string, TableRow>
+ * }} TableRow
+ */
+
+/**
+ * @param {object} params
+ * @param {TableRow[]} params.rows
+ * @param {(id: string) => void} params.onDeleteRow
+ */
+const DataTableContainer = ({ rows, onDeleteRow }) => {
+  const contextValue = React.useMemo(() => ({
+    deleteRow: onDeleteRow
+  }), [onDeleteRow])
+
+  return (
+    <context.Provider value={contextValue}>
+      <DataTable rows={rows} />
+      xxx
+    </context.Provider>
+  )
+}
+
+export default DataTableContainer

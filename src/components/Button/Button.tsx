6e285c92506bd05da0b013fcf893c9b0b22b5535
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-09-25 18:51:19 +0200

    css updates

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,26 @@
+import styled from 'styled-components'
+import tw from 'twin.macro'
 import { ButtonProps } from './types'
 
 const Button = ({ hidden, onClick, children }: ButtonProps) => {
   return (
-    <button
+    <StyledButton
       hidden={hidden}
       onClick={onClick}
-      className="border border-black py-0.5 px-2.5 rounded-xl"
+      className=""
     >
       {children}
-    </button>
+    </StyledButton>
   )
 }
+
+const StyledButton = styled.button`
+  ${tw`
+    border border-gray-500
+    py-1 px-2.5
+    rounded-xl
+    uppercase text-xs font-bold
+  `}
+`
 
 export default Button
